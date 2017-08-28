047ec33f3db08b0abf499b61e7eee550966e42d9
thiagobarso@gmail.com <thiagobarso@gmail.com>
2017-08-28 16:51:48 -0300

    ultimos ajustes

--- app.js
+++ app.js
@@ -18,4 +18,31 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		console.log('Usuário desconectou');
 	});
+
+	socket.on('msgParaServidor', function(data){
+		
+		/* dialogo */
+		socket.emit(
+			'msgParaCliente', 
+			{apelido : data.apelido, mensagem : data.mensagem}
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente',
+			{apelido : data.apelido, mensagem : data.mensagem}
+		);
+
+		/* participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente', 
+				{apelido : data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente',
+				{apelido : data.apelido}
+			);
+		}
+	});
 });
